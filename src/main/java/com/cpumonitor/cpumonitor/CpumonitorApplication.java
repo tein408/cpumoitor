a83a20f047fd09983d281a8a0956755cd0270a69
Eunha Kang <75615404+tein408@users.noreply.github.com>
2024-05-22 12:41:45 +0900

    feat: initial setting

---
+++ src/main/java/com/cpumonitor/cpumonitor/CpumonitorApplication.java
@@ -0,0 +1,13 @@
+package com.cpumonitor.cpumonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpumonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpumonitorApplication.class, args);
+	}
+
+}
