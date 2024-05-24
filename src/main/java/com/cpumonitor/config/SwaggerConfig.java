5b470ef0a5d9593278ac08d95fa0487d889218b4
Eunha Kang <75615404+tein408@users.noreply.github.com>
2024-05-25 00:53:36 +0900

    feat: add swagger setting

---
+++ src/main/java/com/cpumonitor/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.cpumonitor.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.Contact;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.cpumonitor"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("CPU Usage Monitoring API")
+                .description("CPU 사용률을 조회하는 API 입니다.")
+                .version("1.0.0")
+                .contact(new Contact("강은하", "https://www.linkedin.com/in/eunhakang/", "tein408@gmail.com"))
+                .build();
+    }
+
+}
